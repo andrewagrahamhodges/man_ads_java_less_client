26e82a9af2b075a0a8d31bac85b24e19929b5488
Development Manconsulting <32471361+developmentmanconsulting@users.noreply.github.com>
2018-06-18 09:16:12 -0600

    [IMP] Win64 installer

--- createwindows64installer.js
+++ createwindows64installer.js
@@ -1,13 +1,14 @@
 var electronInstaller = require('electron-winstaller');
 
-
-resultPromise1 = electronInstaller.createWindowsInstaller({
+resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: 'packages/MANOVDClient-win32-x64',
     outputDirectory: 'packages/MANOVDClient-win32-x64_installer',
     authors: 'MAN Consulting',
     //exe: 'man_ovd_client.exe',
     setupIcon: "icon.ico",
+    loadingGif:"installing_splash.gif",
+    skipUpdateIcon: true,
     setupExe: 'Install.exe'
   });
 
-resultPromise1.then(() => console.log("Win64 Installer!"), (e) => console.log(`No dice: ${e.message}`));
+resultPromise.then(() => console.log("Win32 Installer!"), (e) => console.log(`No dice: ${e.message}`));
